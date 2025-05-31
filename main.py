b8002b1f6da4e42df51c9492fe05ae9f87b14d85
fahad-islam <fahadislam.dev@gmail.com>
2025-05-31 10:20:29 +0500

    L6: Data Analysis, Count Characters

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_characters
 
 def main():
     num_words = get_num_words()
     print(f"{num_words} words found in the document")
-
+    print(get_num_characters())
 main()
--- stats.py
+++ stats.py
@@ -1,8 +1,20 @@
+path_to_file = "./books/frankenstein.txt"
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def get_num_words():
-    book_text = get_book_text("./books/frankenstein.txt")
+    book_text = get_book_text(path_to_file)
     return len(book_text.split())
+
+def get_num_characters():
+    book_text_character = list(get_book_text(path_to_file).lower())
+    book_characters_dict = {}
+
+    for character in book_text_character:
+        if character in book_characters_dict:
+            book_characters_dict[character] = book_characters_dict[character] + 1
+        else:
+            book_characters_dict[character] = 1
+    return book_characters_dict
