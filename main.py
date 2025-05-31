8c6043dea100188621993a1eead6f2e2d52358ae
fahad-islam <fahadislam.dev@gmail.com>
2025-05-31 10:48:37 +0500

    L1: Report, Print a report

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from stats import get_num_words, get_num_characters
 
 def main():
     num_words = get_num_words()
-    print(f"{num_words} words found in the document")
-    print(get_num_characters())
+    char_counts = get_num_characters()
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    # Sort character counts and print them
+    sorted_chars = sorted(char_counts.items(), key=lambda item: item[1], reverse=True)
+    for char, count in sorted_chars:
+        if char.isalpha():
+            print(f"{char}: {count}")
+
+    print("============= END ===============")
 main()
